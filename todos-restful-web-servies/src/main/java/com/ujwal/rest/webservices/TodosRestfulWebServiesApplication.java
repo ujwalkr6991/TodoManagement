44d6b8f97113a12f8dd2989777d27825c7756ec7
ujwal kumar <ujwal kumar@prince>
2019-07-30 00:04:53 +0530

    1st commit

---
+++ todos-restful-web-servies/src/main/java/com/ujwal/rest/webservices/TodosRestfulWebServiesApplication.java
@@ -0,0 +1,13 @@
+package com.ujwal.rest.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosRestfulWebServiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosRestfulWebServiesApplication.class, args);
+	}
+
+}
---
+++ todos-restful-web-servies/src/test/java/com/ujwal/rest/webservices/TodosRestfulWebServiesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ujwal.rest.webservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TodosRestfulWebServiesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
